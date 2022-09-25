9a44dec2051eea44f92ace84a5594832cf78bc02
Taiye <taiyeajimati@gmail.com>
2022-09-25 23:13:59 +0400

    Last home commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <h1 className="text-3xl bg-gradient-to-br  from-[#23AFFF] via[#36BDF6] to-[#64DFDF] font-bold border-t-4 border-b-4 py-4 ">
+      Hello world!
+    </h1>
+
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#23AFFF',
+      'grey-100': '#ECF0F1',
+      'grey-300': '#BDC3C7',
+      'grey-header': '#3B4452',
+      'grey-body': '#7F8C8D',
+      'grey-blue': '#B5CAD3',
+
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
