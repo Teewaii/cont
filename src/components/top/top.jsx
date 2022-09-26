0a8f4adc78e9ab58f4087f77ed4ab74d0e074df5
Taiye <taiyeajimati@gmail.com>
2022-09-27 00:30:47 +0400

    Last home commit

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -15,8 +15,8 @@ export default function top() {
                 </ul>
 
                 <div className="flex items-center searchbar ">
-                    <input className='w-[257px] py-[10px] focus:outline-none border-2 border-grey300 pl-[20px] pr-[34px] rounded-tl-[5px] rounded-bl-[5px]' type="text" placeholder='Find services for your business' />
-                    <button className='bg-blue hover:bg-blueHover ease-in delay-100 p-2 w-[48px] h-[48px] flex items-center justify-center rounded-tr-[5px] rounded-br-[5px]'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-6 h-6">
+                    <input className='w-[257px] py-[10px] focus:outline-none border-[1px] border-grey300 pl-[20px] pr-[34px] rounded-tl-[5px] rounded-bl-[5px]' type="text" placeholder='Find services for your business' />
+                    <button className='bg-blue hover:bg-blueHover ease-in delay-100 p-2 w-[47px] h-[47px] flex items-center justify-center rounded-tr-[5px] rounded-br-[5px]'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                     </button>
