34a51e2c25eaacbd1cb5ed6e623665164ea3d690
Taiye Ajimati <taiyeajimati@gmail.com>
2022-09-29 18:09:59 +0400

    lastt commit

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -5,7 +5,7 @@ export default function Reserve() {
     <div className='mt-[10px] flex justify-between'>
         <span className='apperance: none'></span>
         <button className='bg-blue rounded-[5px] hover:opacity-80 
-         py-[9px] px-[20px] text-[white] text-[14px] font-semibold tracking-[0.01em]'>Reserver</button>
+         py-[9px] px-[20px] text-[white] text-[14px] font-semibold tracking-[0.01em]'>Reserve</button>
     </div>
   )
 }
